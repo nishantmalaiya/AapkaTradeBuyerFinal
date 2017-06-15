5ad7e17064e300219aac8d1f61389b781ce8fab0
nishantmalaiya <nishantmalaiya@gmail.com>
2017-06-15 18:23:11 +0530

    Resolved conflict dashboard Fragment

--- app/src/main/java/com/aapkatrade/buyer/seller/selleruser_dashboard/billhistory/BillHistoryListHolder.java
+++ app/src/main/java/com/aapkatrade/buyer/seller/selleruser_dashboard/billhistory/BillHistoryListHolder.java
@@ -2,14 +2,24 @@ package com.aapkatrade.buyer.seller.selleruser_dashboard.billhistory;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.TextView;
+
+import com.aapkatrade.buyer.R;
 
 /**
  * Created by PPC17 on 13-Jun-17.
  */
 
 class BillHistoryListHolder extends RecyclerView.ViewHolder {
+
+
+    TextView tvBillhistories,tvPaymentStatus,tvBillhistoryAmount;
     public BillHistoryListHolder(View view) {
         super(view);
+        tvBillhistories=(TextView)view.findViewById(R.id.tv_billhistories);
+        tvPaymentStatus=(TextView)view.findViewById(R.id.tvPaymentStatus);
+        tvBillhistoryAmount=(TextView)view.findViewById(R.id.billhistoryamount);
+
 
 
     }
