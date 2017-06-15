d65fa98845285236215af8f27f553f1abbb0d83b
shankitkumar <sankit@aapkatrade.com>
2017-06-15 14:17:45 +0530

    HI

---
+++ app/src/main/java/com/aapkatrade/buyer/seller/selleruser_dashboard/companyshopmgt/addproduct/AddProductActivity.java
@@ -0,0 +1,26 @@
+package com.aapkatrade.buyer.seller.selleruser_dashboard.companyshopmgt.addproduct;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.aapkatrade.buyer.R;
+import com.aapkatrade.buyer.uicomponent.daystile.DaysTileView;
+
+public class AddProductActivity extends AppCompatActivity {
+    private DaysTileView daysTileView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_product);
+        daysTileView = (DaysTileView) findViewById(R.id.daysTileView);
+        daysTileView.setBackgroundColor(R.color.green);
+        daysTileView.setDayName("Sunday");
+    }
+
+}
