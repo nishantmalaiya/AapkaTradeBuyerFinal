7b5898033947018f1d85d68dafd6c843e6c3cc4c
Arvind <arvindkumaryadav937@gmail.com>
2017-06-17 18:25:22 +0530

    hi

---
+++ app/src/main/java/com/aapkatrade/buyer/seller/selleruser_dashboard/companyshopmgt/addproduct/ProductImagesData.java
@@ -0,0 +1,17 @@
+package com.aapkatrade.buyer.seller.selleruser_dashboard.companyshopmgt.addproduct;
+
+/**
+ * Created by PPC16 on 21-Feb-17.
+ */
+
+public class ProductImagesData
+{
+    public String image_path,image_url;
+
+    public ProductImagesData(String image_path,String image_url)
+    {
+        this.image_path = image_path;
+        this.image_url = image_url;
+    }
+
+}
